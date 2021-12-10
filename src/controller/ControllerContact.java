8abeac95f62c356a106d510fd5863ad83ece7d75
unknown <dahxjd@gmail.com>
2021-12-10 10:59:58 +0700

    md2

---
+++ src/controller/ControllerContact.java
@@ -0,0 +1,52 @@
+package controller;
+
+import IO.ReadAndWrite;
+import jdk.nashorn.internal.parser.Scanner;
+import model.Contact;
+import service.ContactService;
+import view.ViewContact;
+
+public class ControllerContact {
+    ReadAndWrite readAndWrite = new ReadAndWrite();
+    ContactService contactService = new ContactService();
+    ViewContact viewContact = new ViewContact();
+
+    public void controllerContact() {
+
+        while (true) {
+            int choice = viewContact.menuManagerContact();
+            if (choice >= 1 && choice <= 8) {
+                switch (choice) {
+                    case 1:
+                        contactService.showListContact();
+                        break;
+                    case 2:
+                        contactService.addList(viewContact.menuAddList());
+                        break;
+                    case 3:
+                        contactService.showListContact();
+                        String name = viewContact.editByName();
+                        Contact contact = viewContact.menuEditContact(name);
+                        contactService.editContact(name, contact);
+                        break;
+                    case 4:
+                        contactService.showListContact();
+                        contactService.deleteByNmae(viewContact.menudeleteByName());
+                        break;
+                    case 5:
+                        contactService.searchByName(viewContact.menusearchByName());
+                        break;
+                    case 6:
+                        readAndWrite.realdFile();
+                        break;
+                    case 7:
+                        readAndWrite.writeToFile();
+                        break;
+                    case 8:
+                        System.exit(0);
+                }
+            } else
+                System.err.println(" lựa chọn không có trong danh sách");
+        }
+    }
+}
---
+++ src/controller/Main.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Main {
+    public static void main(String[] args) {
+        ControllerContact controllerContact = new ControllerContact();
+        controllerContact.controllerContact();
+    }
+}
